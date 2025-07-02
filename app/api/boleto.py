2aa3a99274a64ed30737e43b1353208755914ead
DandaraEmiliano <emilianodandara@gmail.com>
2025-07-02 15:52:43 -0300

    feat: geração de PDF para boletos

--- app/api/boleto.py
+++ app/api/boleto.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.schema.boleto import BoletoCreate, BoletoResponse
 from app.services.boleto_service import gerar_boleto, buscar_boleto_por_codigo
+from fastapi.responses import Response
+from app.utils.pdf_generator import gerar_pdf_boleto
 
 
 router = APIRouter()
@@ -14,5 +16,14 @@ def criar_boleto(boleto: BoletoCreate):
 def reemitir_boleto(codigo: str):
     try:
         return buscar_boleto_por_codigo(codigo)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    
+@router.get("/boletos/{codigo}/pdf")
+def gerar_pdf(codigo: str):
+    try:
+        boleto = buscar_boleto_por_codigo(codigo)
+        pdf_bytes = gerar_pdf_boleto(boleto.dict())
+        return Response(content=pdf_bytes, media_type="application/pdf")
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
